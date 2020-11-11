174ab569f45d402b6c4754d774bcaa0aa45c3092
kaixing2012 <59598698+kaixing2012@users.noreply.github.com>
2020-11-11 08:17:32 +0800

    add navigator

--- angular/src/app/shared/components/navigator/navigator.component.ts
+++ angular/src/app/shared/components/navigator/navigator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigator',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigatorComponent implements OnInit {
 
+  isMobileSize: boolean = false;
+  private innerWidth: number
+
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth >= 767.98) {
+      this.isMobileSize = false;
+    }
+    else {
+      this.isMobileSize = true;
+    }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+
+    if (event != undefined) {
+      this.innerWidth = event.target.innerWidth;
+    }
+    else {
+      this.innerWidth = document.body.clientWidth;
+    }
+
+    if (this.innerWidth >= 767.98) {
+      this.isMobileSize = false;
+    }
+    else {
+      this.isMobileSize = true;
+    }
+  }
 }
