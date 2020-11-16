cb440e64d5073b634e2382b3a94ac323923796e2
kaixing2012 <59598698+kaixing2012@users.noreply.github.com>
2020-11-16 22:03:17 +0800

    add self introduction

--- angular/src/app/modules/home/home.component.ts
+++ angular/src/app/modules/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,19 +7,53 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('coverFrame') coverFrame: ElementRef
+  @ViewChild('frontFrame') frontFrame: ElementRef
+  @ViewChild('selfie') selfie: ElementRef
+  @ViewChild('detail') detail: ElementRef
+
+  isMobileMode: boolean
+  winInnerWidth: number = window.innerWidth;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event) {
-    let div = document.getElementById('test')
-    if (div.getBoundingClientRect().top < 0) {
-      document.getElementById('tes2').style.backgroundImage = "url(/assets/images/home-dark-row.jpg)"
-    } else {
-      document.getElementById('tes2').style.backgroundImage = "url(/assets/images/home-cover.jpg)";
+    if (this.winInnerWidth >= 575.98) {
+      this.isMobileMode = false;
+    }
+    else {
+      this.isMobileMode = true;
     }
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    if (this.frontFrame.nativeElement.getBoundingClientRect().top < 0) {
+      this.coverFrame.nativeElement.style.backgroundImage = "url(/assets/images/home-dark-row.jpg)"
+      if (!this.isMobileMode) {
+        this.selfie.nativeElement.style.display = "inline"
+        this.detail.nativeElement.style.display = "inline"
+
+        setTimeout(() => {
+          this.selfie.nativeElement.style.transform = "scale(.7)"
+          this.detail.nativeElement.style.transform = "scale(.7)"
+
+        }, 100);
+      }
+
+    } else {
+      this.coverFrame.nativeElement.style.backgroundImage = "url(/assets/images/home-cover.jpg)";
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    let target = event.target as Window
+    if (target.innerWidth >= 575.98) {
+      this.isMobileMode = false;
+    }
+    else {
+      this.isMobileMode = true;
+    }
+  }
 }
