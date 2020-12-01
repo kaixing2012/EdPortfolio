21df9639e5fb57e0e8636e6a6f325cce6410f4bd
kaixing2012 <59598698+kaixing2012@users.noreply.github.com>
2020-12-01 23:58:56 +0800

    init shop module

--- angular/src/app/modules/routing/app-routing.module.ts
+++ angular/src/app/modules/routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ShoppingCarComponent } from '../shopping-car/shopping-car.component';
+import { ShopComponent } from '../shop/shop.component';
 import { LifestyleComponent } from '../lifestyle/lifestyle.component';
 import { HomeComponent } from 'src/app/modules/home/home.component';
 import { MapComponent } from '../map/map.component';
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'map', component: MapComponent },
-  { path: 'shopping-car', component: ShoppingCarComponent },
+  { path: 'shop', component: ShopComponent },
   { path: 'lifestyle', component: LifestyleComponent },
 ];
 
--- angular/src/app/modules/shopping-car/shopping-car.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ShoppingCarComponent } from './shopping-car.component';
-
-@NgModule({
-  declarations: [ShoppingCarComponent],
-  imports: [CommonModule],
-})
-export class ShoppingCarModule {}
