d1d16d07b9ab104d4ca6310540a67dda544b2aab
kaixing2012 <59598698+kaixing2012@users.noreply.github.com>
2020-12-15 16:18:28 +0800

    override create method in veiw_api of wonder app

--- django/modules/map/app_wonder/views_api.py
+++ django/modules/map/app_wonder/views_api.py
@@ -10,3 +10,11 @@ from .serializers import WonderSerializer
 class WonderAPIViewSet(viewsets.ModelViewSet):
     queryset = Wonder.objects.all()
     serializer_class = WonderSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data=request.data, many=isinstance(request.data, list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
