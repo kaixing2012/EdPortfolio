520bccdeca07531bcf31c8a3a0cdb66f4a60a9ae
kaixing2012 <59598698+kaixing2012@users.noreply.github.com>
2020-12-17 10:43:51 +0800

    start app shopping item and shopping cart

---
+++ django/modules/shop/app_shopping_cart/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppShoppingCartConfig(AppConfig):
+    name = 'app_shopping_cart'
---
+++ django/modules/shop/app_shopping_item/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppShoppingItemConfig(AppConfig):
+    name = 'app_shopping_item'
